231afc87037ad5cb8b5a7688fe793f683ccdd0c1
m <m@marvelmathew.ca>
2016-02-05 06:58:17 -0600

    Use Application

--- lib/wacha.ex
+++ lib/wacha.ex
@@ -1,3 +1,8 @@
 defmodule Wacha do
-  
+  use Application
+
+  def start(_type, _args) do
+    IO.puts "Hello world"
+    Task.start(fn -> :timer.sleep(1000); IO.puts("done sleeping") end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Wacha.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger,
+    [mod: {Wacha, []},
+     applications: [:logger,
                     :httpoison]]
   end
 
